f380c9e7af0fbbeaccb4612dc2c8bbe536d7d536
Conor Reid <conor.reid@ucdconnect.ie>
2018-03-02 13:15:44 +0000

    remove old dir

--- EDA/eebp.r
+++
@@ -1,9 +0,0 @@
-require(reshape)
-library(stringr)
-library(rpart)
-setwd("/Users/Conor/Google Drive/MSc/dissertation/data/processed")
-
-#######Analysis
-#eastern europe 300
-eebp = read.csv("eebp.csv", sep=";", na.strings="?")
-summary(eebp)
