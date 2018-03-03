94126e30f340917f0c29efec2dfba440f6329689
Conor Reid <conor.reid@ucdconnect.ie>
2018-03-03 14:57:14 +0000

    rework adaboost function to take target as parameter, reduce amount of results returned. do that analysis in a separate system

--- cgcp/analysis/replicateMM.R
+++ cgcp/analysis/replicateMM.R
@@ -21,15 +21,16 @@ training.split=2/3 #proportion of data that will be dedicated to training data s
 #   
 # Returns:
 #   A number of elements of the results of adaboost
-adaboost <- function(dataset) {
+adaboost <- function(dataset, target) {
   drops <- c("Ticker",
              "AZS.class",
+             "AZS",
              "Tobins.Q",
-             "AZS")
+             "Tobins.Q.class")
+  drops=drops[drops != target]#dont want to remove whatever is passed as the target
   data.reduced<-dataset[ , !(names(dataset) %in% drops)] #remove unwanted columns
-  data.reduced<-data.reduced[complete.cases(data.reduced[ , "Tobins.Q.class"]),]# we only want records with a class indicator
+  data.reduced<-data.reduced[complete.cases(data.reduced[ , target]),]# we only want records with a class indicator
   
-  data.reduced$Tobins.Q.class=as.factor(data.reduced$Tobins.Q.class)
   data.reduced$Feml.CEO.or.Equiv=as.numeric(as.factor(data.reduced$Feml.CEO.or.Equiv))
   data.reduced$Prsdg.Dir=as.numeric(as.factor(data.reduced$Prsdg.Dir))
   data.reduced$Clssfd.Bd.Sys=as.numeric(as.factor(data.reduced$Clssfd.Bd.Sys))
@@ -38,11 +39,15 @@ adaboost <- function(dataset) {
   data.reduced$Indep.Chrprsn=as.numeric(as.factor(data.reduced$Indep.Chrprsn))
   data.reduced$Frmr.CEO.or.its.Equiv.on.Bd=as.numeric(as.factor(data.reduced$Frmr.CEO.or.its.Equiv.on.Bd))
   
+  data.reduced[[target]]=as.factor(data.reduced[[target]])
+  colnames(data.reduced)[colnames(data.reduced) == target] <- 'target'
+  
   len <- length(data.reduced[,1])
   sub <- sample(1:len,len*training.split)
   mfinal <- 100 #default is 100
   maxdepth <- 30 #30 is max
-  data.adaboost <- boosting(Tobins.Q.class~., 
+  data.reduced<-data.frame(data.reduced)
+  data.adaboost <- boosting(target~., 
                            data=data.reduced[sub, ], 
                            boos=TRUE, 
                            mfinal=mfinal,
@@ -53,49 +58,35 @@ adaboost <- function(dataset) {
   errorevol(data.adaboost,newdata=data.reduced[sub, ])->evol.train
   errorevol(data.adaboost,newdata=data.reduced[-sub, ])->evol.test
   
-  true.negative<-data.adaboost.pred$confusion[1]   
-  false.positive<-data.adaboost.pred$confusion[2]
-  false.negative<-data.adaboost.pred$confusion[3] 
-  true.positive<-data.adaboost.pred$confusion[4] 
-  
-  # > precision
-  precision.class0=true.negative/(true.negative + false.negative)
-  precision.class1=true.positive/(true.positive + false.positive)
-  
-  # > recall
-  recall=true.positive/(true.positive + false.negative)
-  
-  # > ROC and AUC
-  roc.obj <- roc(data.reduced[-sub,]$Tobins.Q.class, as.numeric(data.adaboost.pred$class))
-  
   result=list(
+     "data.reduced"=data.reduced, 
+     "model"=data.adaboost,
+     "model.pred"=data.adaboost.pred,
      "confusion"=data.adaboost.pred$confusion,
      "error"=data.adaboost.pred$error,
      "evol.train"=evol.train,
-     "evol.test"=evol.test,
-     "trueNegative"=true.negative,
-     "falsePositive"=false.positive,
-     "falseNegative"=false.negative,
-     "truePositive"=true.positive,
-     "precision.class0"=precision.class0,
-     "precision.class1"=precision.class1,
-     "recall"=recall,
-     "roc.obj"=roc.obj
+     "evol.test"=evol.test
   )
   return(result)  
 }
 #call adaboost on each
-spx.adaboost.results=adaboost(spx)
-sxxp.adaboost.results=adaboost(sxxp)
-eebp.adaboost.results=adaboost(eebp)
+spx.adaboost.tobin.results=adaboost(spx,"Tobins.Q.class")
+spx.adaboost.altman.results=adaboost(spx,"AZS.class")
+sxxp.adaboost.tobin.results=adaboost(sxxp,"Tobins.Q.class")
+sxxp.adaboost.altman.results=adaboost(sxxp,"AZS.class")
+eebp.adaboost.tobin.results=adaboost(eebp,"Tobins.Q.class")
+eebp.adaboost.altman.results=adaboost(eebp,"AZS.class")
+
+
 #analyze the results
-c(spx.adaboost.results$precision.class0, spx.adaboost.results$precision.class1,auc(spx.adaboost.results$roc.obj))
-c(sxxp.adaboost.results$precision.class0, sxxp.adaboost.results$precision.class1,auc(sxxp.adaboost.results$roc.obj))
-c(eebp.adaboost.results$precision.class0, eebp.adaboost.results$precision.class1,auc(eebp.adaboost.results$roc.obj))
+#c(spx.adaboost.tobin.results$precision.class0, spx.adaboost.tobin.results$precision.class1,auc(spx.adaboost.tobin.results$roc.obj))
+#c(sxxp.adaboost.tobin.results$precision.class0, sxxp.adaboost.tobin.results$precision.class1,auc(sxxp.adaboost.tobin.results$roc.obj))
+#c(eebp.adaboost.tobin.results$precision.class0, eebp.adaboost.tobin.results$precision.class1,auc(eebp.adaboost.tobin.results$roc.obj))
 # > errors in train and test
-plot.errorevol(spx.adaboost.results$evol.test,spx.adaboost.results$evol.train)
-plot.errorevol(sxxp.adaboost.results$evol.test,sxxp.adaboost.results$evol.train)
-plot.errorevol(eebp.adaboost.results$evol.test,eebp.adaboost.results$evol.train)
+#plot.errorevol(spx.adaboost.tobin.results$evol.test,spx.adaboost.tobin.results$evol.train)
+#plot.errorevol(sxxp.adaboost.tobin.results$evol.test,sxxp.adaboost.tobin.results$evol.train)
+#plot.errorevol(eebp.adaboost.tobin.results$evol.test,eebp.adaboost.tobin.results$evol.train)
+
 
 
 #**********************
@@ -153,4 +144,30 @@ eebp.j48.results=j48(eebp)
 auc(spx.j48.results$roc.obj)
 auc(sxxp.j48.results$roc.obj)
 auc(eebp.j48.results$roc.obj)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+df <- data.frame(A=1:10, B=2:11, C=3:12)
+fun1 <- function(x, column){
+  x[,column]
+}
+fun1(df, "B")
+
+
 
