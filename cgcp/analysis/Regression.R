f2c0c74969b9501d8c5c8445f8c26243cb0090de
Conor Reid <conor.reid@ucdconnect.ie>
2018-05-07 20:02:21 +0100

    first attempt at reg LR

--- cgcp/analysis/Regression.R
+++ cgcp/analysis/Regression.R
@@ -6,14 +6,6 @@ mydb <- dbConnect(MySQL(), user='root', password='', dbname='corp_gov_processed'
 spx <- dbReadTable(conn=mydb,name='spx')
 
 training.split=2/3
-
-#perform some variable selection
-variableSelection <- function(dataset, target){
-  
-  
-}
-
-
 #multiple linear regression
 #dependent variable is continuous, independent variables are continuous/discrete, regression line is linear.
 #http://r-statistics.co/Linear-Regression.html
@@ -34,7 +26,7 @@ multipleLinearRegressionWithoutImputation <- function(dataset, target){
   drops <- drops[drops != target]#dont want to remove whatever is passed as the target
   data.reduced <- dataset[ , !(names(dataset) %in% drops)] #remove unwanted columns
   data.reduced <- data.reduced[complete.cases(data.reduced[ , target]),]# we only want records with a class indicator
-  data.reduced[[target]] <- as.numeric(as.factor(data.reduced[[target]]))
+  #data.reduced[[target]] <- as.numeric(as.factor(data.reduced[[target]]))
   colnames(data.reduced)[colnames(data.reduced) == target] <- 'target'
   data.reduced <- data.frame(data.reduced)
   
@@ -67,9 +59,10 @@ multipleLinearRegressionWithoutImputation <- function(dataset, target){
 }
 multipleLinearRegressionWithoutImputation(spx,"Tobins.Q")
 
-library(e1071)
-library(mice)
+
 linearRegressionWithImputationMice <- function(dataset, target) {
+  library(e1071)
+  library(mice)
   imputation.maxit=15
   imputation.m=5
   
@@ -90,7 +83,7 @@ linearRegressionWithImputationMice <- function(dataset, target) {
   drops <- drops[drops != target]#dont want to remove whatever is passed as the target
   data.reduced <- dataset[ , !(names(dataset) %in% drops)] #remove unwanted columns
   data.reduced <- data.reduced[complete.cases(data.reduced[ , target]),]# we only want records with a class indicator
-  data.reduced[[target]] <- as.numeric(as.factor(data.reduced[[target]]))
+  #data.reduced[[target]] <- as.numeric(as.factor(data.reduced[[target]]))
   colnames(data.reduced)[colnames(data.reduced) == target] <- 'target'
   data.reduced <- data.frame(data.reduced)
   
@@ -154,15 +147,89 @@ linearRegressionWithImputationMice <- function(dataset, target) {
   #https://stats.stackexchange.com/questions/287143/meaning-of-min-max-accuracy-of-a-regression-model?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa
   min.max.accuracy <- mean(apply(pred.actuals, 1, min) / apply(pred.actuals, 1, max)) 
   print(min.max.accuracy)
-
+  
 }  
-linearRegressionWithImputationMice(spx,"AZS")#eg 0.6423513
-linearRegressionWithImputationMice(spx,"Tobins.Q")#0.6761327
-
-
+linearRegressionWithImputationMice(spx,"AZS")#eg 0.5118446
+linearRegressionWithImputationMice(spx,"Tobins.Q")#eg 0.6871641
 
 
 #regularised linear regression
+library(glmnet)
+regLinearRegression <- function(dataset, target){
+  #https://educationalresearchtechniques.com/2017/04/05/8601/
+  #some data manipulations
+  drops <- c("Ticker",
+             "AZS.class", #one of these (the target) will be added back in
+             "AZS", #one of these (the target) will be added back in
+             "Tobins.Q", #one of these (the target) will be added back in
+             "Tobins.Q.class", #one of these (the target) will be added back in
+             "X..Indep.Dir.on.Comp.Cmte.1", #basically no variance
+             "X..Indep.Dir.on.Aud.Cmte.1", #basically no variance
+             "ROC", #too many missing values
+             "Bd.Age.Limit", #too many missing values
+             "Exec.Dir.Bd.Dur", #causing errors in pooling(?)
+             "Unit.or.2.Tier.Bd.Sys", #causing errors in pooling(?)
+             "Age.Old.Dir" #causing errors in pooling(?)
+  )
+  drops <- drops[drops != target]#dont want to remove whatever is passed as the target
+  data.reduced <- dataset[ , !(names(dataset) %in% drops)] #remove unwanted columns
+  data.reduced <- data.reduced[complete.cases(data.reduced[ , target]),]# we only want records with a class indicator
+  #data.reduced[[target]] <- as.numeric(as.factor(data.reduced[[target]]))
+  colnames(data.reduced)[colnames(data.reduced) == target] <- 'target'
+  data.reduced <- data.frame(data.reduced)
+  
+  data.reduced$Feml.CEO.or.Equiv <- as.numeric(as.factor(data.reduced$Feml.CEO.or.Equiv))
+  data.reduced$Prsdg.Dir <- as.numeric(as.factor(data.reduced$Prsdg.Dir))
+  data.reduced$Clssfd.Bd.Sys <- as.numeric(as.factor(data.reduced$Clssfd.Bd.Sys))
+  data.reduced$Indep.Lead.Dir <- as.numeric(as.factor(data.reduced$Indep.Lead.Dir))
+  data.reduced$CEO.Duality <- as.numeric(as.factor(data.reduced$CEO.Duality))
+  data.reduced$Indep.Chrprsn <- as.numeric(as.factor(data.reduced$Indep.Chrprsn))
+  data.reduced$Frmr.CEO.or.its.Equiv.on.Bd <- as.numeric(as.factor(data.reduced$Frmr.CEO.or.its.Equiv.on.Bd))
+  
+  #to check the balance of the target variable
+  #plot(density(data.reduced$target), main="Density Plot", ylab="Frequency", sub=paste("Skewness:", round(e1071::skewness(data.reduced$target), 2))) 
+  #polygon(density(data.reduced$target), col="red")
+  
+  #split dataset into training and test
+  #md.pattern(data.reduced)
+  #library(VIM) #for visualizations of the magnitude of missing values
+  len <- length(data.reduced[,1])
+  sub <- sample(1:len,len*training.split)
+  data.reduced.train <- data.frame(data.reduced[sub,])
+  data.reduced.test <- data.frame(data.reduced[-sub,])
+  
+  data.reduced.train.complete=data.reduced.train[complete.cases(data.reduced.train), ]
+  data.reduced.test.complete=data.reduced.test[complete.cases(data.reduced.test), ]
+  
+  #fit on the training set
+  predictor.variables <- as.matrix(data.reduced.train.complete[ , !(names(data.reduced.train.complete) %in% "target")]) #remove unwanted columns
+  target.measure <- as.matrix(data.reduced.train.complete$target)
+  lasso <- glmnet(
+    predictor.variables,
+    target.measure,
+    family="gaussian",
+    alpha=1
+  )
+  print(lasso)
+  plot(lasso,xvar="lambda",label=T)
+  plot(lasso,xvar='dev',label=T)
+  
+  #test
+  data.reduced.test.complete.without.target <- as.matrix(data.reduced.test.complete[ , !(names(data.reduced.test.complete) %in% "target")]) #remove unwanted columns
+  lasso.response <- predict(
+    lasso,
+    newx = data.reduced.test.complete.without.target,
+    type = 'response',
+    s=.007
+  )
+  plot(lasso.response,data.reduced.test.complete$target)
+  lasso.resid <- lasso.response-data.reduced.test.complete$target
+  print(mean(lasso.resid^2))
+  
+}
+regLinearRegression(spx,"Tobins.Q")#eg 0.655467
+
+
 
 #2. apply classification on regression
 #3. results analysis and discussion
