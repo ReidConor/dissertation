09a3f519ad0c81834c60f35c3ea732723189c516
Conor Reid <conor.reid@ucdconnect.ie>
2018-05-07 17:21:18 +0100

    finished off mlr with imputation, shell for variable selection

--- cgcp/analysis/Regression.R
+++ cgcp/analysis/Regression.R
@@ -6,6 +6,14 @@ mydb <- dbConnect(MySQL(), user='root', password='', dbname='corp_gov_processed'
 spx <- dbReadTable(conn=mydb,name='spx')
 
 training.split=2/3
+
+#perform some variable selection
+variableSelection <- function(dataset, target){
+  
+  
+}
+
+
 #multiple linear regression
 #dependent variable is continuous, independent variables are continuous/discrete, regression line is linear.
 #http://r-statistics.co/Linear-Regression.html
@@ -62,6 +70,10 @@ multipleLinearRegressionWithoutImputation(spx,"Tobins.Q")
 library(e1071)
 library(mice)
 linearRegressionWithImputationMice <- function(dataset, target) {
+  imputation.maxit=15
+  imputation.m=5
+  
+  #some data manipulations
   drops <- c("Ticker",
              "AZS.class", #one of these (the target) will be added back in
              "AZS", #one of these (the target) will be added back in
@@ -94,52 +106,61 @@ linearRegressionWithImputationMice <- function(dataset, target) {
   #plot(density(data.reduced$target), main="Density Plot", ylab="Frequency", sub=paste("Skewness:", round(e1071::skewness(data.reduced$target), 2))) 
   #polygon(density(data.reduced$target), col="red")
   
-  #to impute
+  #split dataset into training and test
   #md.pattern(data.reduced)
   #library(VIM) #for visualizations of the magnitude of missing values
   len <- length(data.reduced[,1])
   sub <- sample(1:len,len*training.split)
   data.reduced.train <-  data.frame(data.reduced[sub,])
   data.reduced.test <- data.frame(data.reduced[-sub,])
-    
-  data.reduced.train.imputed <- mice(data = data.reduced.train, m = 5, method = "pmm", maxit = 1, seed = 500)
-  training.models <- list(analyses=vector("list", data.reduced.train.imputed$m))
-
-  print(typeof(data.reduced.train.imputed)) 
-  #build the model
-  #data.reduced.imputed$m is 5 here, or the m param in the mice command above
-  for(i in 1:data.reduced.train.imputed$m){
-    training.models$analyses[[i]] <- lm(target ~ Tax + Norm.NI.to.NI.for.Cmn.. + Interest + OPM.T12M + 
-                              Asset + Fincl..l + Oper.ROE + X5Yr.Avg.Adj.ROE + Dvd.P.O + 
-                            Sust.Gr.Rt + EBITDA.Sh + P.E + EPS + P.B + P.EBITDA + ROE + 
-                              EV.EBITDA.T12M + Net.Debt.to.EBITDA + Cash.Gen.Cash.Reqd + 
-                             Dvd.Yld + Board.Size + X..Empl.Reps.on.Bd + 
-                             Clssfd.Bd.Sys + X..Non.Exec.Dir.on.Bd + X..NonExec.Dir.on.Bd + 
-                              X..Indep.Directors + Indep.Directors + CEO.Duality + Indep.Chrprsn + 
-                              Indep.Lead.Dir + Prsdg.Dir + Frmr.CEO.or.its.Equiv.on.Bd + 
-                              X..Women.on.Bd + X..Wmn.on.Bd + X..Feml.Execs + X..Feml.Execs.1 + 
-                             Feml.CEO.or.Equiv + X..Execs...Co.Mgrs + Age.Young.Dir + 
-                             BOD.Age.Rng +  Bd.Avg.Age + Board.Duration + 
-                             Board.Mtgs.. + Board.Mtg.Att.. + Indep.Dir.Bd.Mtg.Att.. + 
-                             Sz.Aud.Cmte + X..Indep.Dir.on.Aud.Cmte + X..Indep.Dir.on.Comp.Cmte + 
-                             X..Indep.Dir.on.Nom.Cmte, data=complete(data.reduced.train.imputed, i))
   
+  #impute the training set independantly
+  #https://stats.stackexchange.com/questions/95083/imputation-before-or-after-splitting-into-train-and-test?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa
+  data.reduced.train.imputed <- mice(data = data.reduced.train, m = imputation.m, method = "pmm", maxit = imputation.maxit, seed = 500)
+  data.reduced.train.imputed.stacked <- complete(data.reduced.train.imputed, 1)
+  for(i in 2:data.reduced.train.imputed$m){
+    data.reduced.train.imputed.stacked <- rbind(data.reduced.train.imputed.stacked, complete(data.reduced.train.imputed, i))
   }
-  class(training.models) <- "mira"
-  combined.training.models <- pool(training.models)
-  #summary(combined.training.models)
-
-  #predict the test set
-  pred <- predict(combined.training.models, data.reduced.test)
-  #pred.actuals <- data.frame(cbind(actuals=data.reduced.test$target, predicteds=pred))
+  data.reduced.train.imputed.stacked.complete=data.reduced.train.imputed.stacked[complete.cases(data.reduced.train.imputed.stacked), ]
+  #print(nrow(data.reduced.train.imputed.stacked)) #theres a mismatch here. mice is returning missing values, which indicates collinearity. might need variable selection 
+  #print(nrow(data.reduced.train.imputed.stacked.complete))
+  
+  #build the model
+  model <- lm(target ~ ., data = data.reduced.train.imputed.stacked.complete)
+  #print(summary(model))
+  #print(coefficients(model)) # model coefficients
+  #print(confint(model, level=0.95)) # CIs for model parameters 
+  #print(fitted(model)) # predicted values
+  #print(residuals(model)) # residuals
+  #print(anova(model)) # anova table 
+  #print(vcov(model)) # covariance matrix for model parameters 
+  #print(influence(model)) # regression diagnostics
+  
+  #now impute the test set
+  data.reduced.test.imputed <- mice(data = data.reduced.test, m = imputation.m, method = "pmm", maxit = imputation.maxit, seed = 500)
+  data.reduced.test.imputed.stacked <- complete(data.reduced.test.imputed, 1)
+  for(i in 2:data.reduced.test.imputed$m){
+    data.reduced.test.imputed.stacked <- rbind(data.reduced.test.imputed.stacked, complete(data.reduced.test.imputed, i))
+  }
+  data.reduced.test.imputed.stacked.complete=data.reduced.test.imputed.stacked[complete.cases(data.reduced.test.imputed.stacked), ]
+  
+  #predict using test set and model
+  pred <- predict(model, data.reduced.test.imputed.stacked.complete)
+  
+  #analysis
+  pred.actuals <- data.frame(cbind(actuals=data.reduced.test.imputed.stacked.complete$target, predicteds=pred))
   #print(cor(pred.actuals))
   #head(pred.actuals)
-  
-  #min.max.accuracy <- mean(apply(pred.actuals, 1, min) / apply(pred.actuals, 1, max)) 
-  #print(min.max.accuracy)
+  #https://stats.stackexchange.com/questions/287143/meaning-of-min-max-accuracy-of-a-regression-model?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa
+  min.max.accuracy <- mean(apply(pred.actuals, 1, min) / apply(pred.actuals, 1, max)) 
+  print(min.max.accuracy)
 
-} 
-linearRegressionWithImputationMice(spx,"Tobins.Q")
+}  
+linearRegressionWithImputationMice(spx,"AZS")#eg 0.6423513
+linearRegressionWithImputationMice(spx,"Tobins.Q")#0.6761327
+
+
+
 
 #regularised linear regression
 
