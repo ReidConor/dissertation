0cedc8dcb792a025f3f10d39f5be1cb25200c06a
Conor Reid <conor.reid@ucdconnect.ie>
2018-05-10 20:50:04 +0100

    placeholder for feature selection WIP

---
+++ cgcp/analysis/featureSelection.R
@@ -0,0 +1,3 @@
+#https://www.analyticsvidhya.com/blog/2016/12/introduction-to-feature-selection-methods-with-an-example-or-how-to-select-the-right-variables/
+#https://towardsdatascience.com/regularization-in-machine-learning-76441ddcf99a
+#https://beta.vu.nl/nl/Images/werkstuk-fonti_tcm235-836234.pdf
