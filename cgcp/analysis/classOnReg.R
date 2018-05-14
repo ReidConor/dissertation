5994c18891d845fc28c3311066bdafd1c4617d5a
Conor Reid <conor.reid@ucdconnect.ie>
2018-05-14 20:46:08 +0100

    placeholder for classification on regression

---
+++ cgcp/analysis/classOnReg.R
@@ -0,0 +1,6 @@
+#!/usr/bin/env Rscript
+#2. apply classification-on-regression 
+#+(i.e. thresholding on the real value predicted by regression) 
+#+rather than straight classification
+
+#see paper "The R Package threg to Implement Threshold Regression Models"
