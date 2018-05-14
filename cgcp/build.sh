4f6328caa84ee48a59c15922772ed28cc53712b4
Conor Reid <conor.reid@ucdconnect.ie>
2018-05-14 20:40:21 +0100

    sort out permissions on R files to run on cmd line

--- cgcp/build.sh
+++ cgcp/build.sh
@@ -7,10 +7,14 @@
 #/
 export jobDir=`pwd`
 export loadDir=`cd "$jobDir"; cd load/; pwd`
+export analysisDir=`cd analysis; pwd`
 
 filePermissions(){
   cd "$loadDir"
   chmod 755 load.sh cleanData.sh replaceBlanks.py
+
+  cd "$analysisDir"
+  chmod 755 *.R
 
 }
 
