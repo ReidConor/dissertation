1aae9f33d6bdc39cc75f90411256d0af84653ea1
Conor Reid <conor.reid@ucdconnect.ie>
2018-05-20 20:39:23 +0100

    add in a comment

--- cgcp/build.sh
+++ cgcp/build.sh
@@ -5,6 +5,8 @@
 #/ Purpose : builds this project
 #/ Create date : 02-03-2018
 #/
+
+#try this https://stackoverflow.com/questions/59895/getting-the-source-directory-of-a-bash-script-from-within?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa
 export jobDir=/Users/Conor/Google\ Drive/MSc/_dissertation/cgcp
 export loadDir=`cd "$jobDir"; cd load/; pwd`
 export analysisDir=`cd "$jobDir"; cd analysis; pwd`
