32b2fb8b0c6cd33bb358accc1836cdac22a1ac9d
Conor Reid <conor.reid@ucdconnect.ie>
2018-05-31 21:48:15 +0100

    imputes and pushes to mysql

---
+++ cgcp/load/impute.R
@@ -0,0 +1,44 @@
+#!/usr/bin/env Rscript
+
+#get data in from mysql
+library(RMySQL)
+mydb_processed <- dbConnect(MySQL(), user='root', password='', dbname='corp_gov_processed')
+spx <- dbReadTable(conn=mydb_processed,name='spx')
+sxxp <- dbReadTable(conn=mydb_processed,name='sxxp')
+eebp <- dbReadTable(conn=mydb_processed,name='eebp')
+
+#impute
+impute <- function(dataset){
+  set.seed(1)
+  library(mice)
+  imputation.maxit <- 15
+  imputation.m <- 3
+  imputation.method <- "pmm"
+
+  dataset.imputed <- mice(
+    data = dataset,
+    m = imputation.m,
+    method = imputation.method,
+    maxit = imputation.maxit,
+    seed = 500
+  )
+  dataset.imputed.stacked <- data.frame()
+  for(i in 1:imputation.m){
+    dataset.imputed.stacked <- rbind(dataset.imputed.stacked, complete(dataset.imputed, i))
+  }
+  return(dataset.imputed.stacked)
+}
+spx.imputed <- impute(spx)
+sxxp.imputed <- impute(sxxp)
+eebp.imputed <- impute(eebp)
+
+#get complete cases
+spx.imputed.complete=spx.imputed[complete.cases(spx.imputed), ]
+sxxp.imputed.complet=sxxp.imputed[complete.cases(sxxp.imputed), ]
+eebp.imputed.complete=eebp.imputed[complete.cases(eebp.imputed), ]
+
+#write back to mysql
+mydb_imputed <- dbConnect(MySQL(), user='root', password='', dbname='corp_gov_imputed')
+dbWriteTable(mydb_imputed, value = spx.imputed.complete, name = "spx", overwrite = TRUE )
+dbWriteTable(mydb_imputed, value = sxxp.imputed.complet, name = "sxxp", overwrite = TRUE )
+dbWriteTable(mydb_imputed, value = eebp.imputed.complete, name = "eebp", overwrite = TRUE )
