528393aa495c20cb58aafaf35a59d6fe3e3dbb6c
Conor Reid <conor.reid@ucdconnect.ie>
2018-06-06 20:12:18 +0100

    more targeted scaling

--- cgcp/load/scaleFromImputed.R
+++ cgcp/load/scaleFromImputed.R
@@ -1,20 +1,42 @@
 #!/usr/bin/env Rscript
 library(RMySQL)
+library(sqldf)
 #get data
 mydb_imputed  <- dbConnect(MySQL(), user='root', password='', dbname='corp_gov_imputed')
-spx<-dbReadTable(conn=mydb_imputed,name='spx')
-
-spx$Feml.CEO.or.Equiv <- (as.numeric(as.factor(spx$Feml.CEO.or.Equiv)))
-spx$AZS.class <- (as.numeric(as.factor(spx$AZS.class)))
-spx$Frmr.CEO.or.its.Equiv.on.Bd <- (as.numeric(as.factor(spx$Frmr.CEO.or.its.Equiv.on.Bd)))
-spx$Prsdg.Dir <- (as.numeric(as.factor(spx$Prsdg.Dir)))
-spx$Indep.Lead.Dir <- (as.numeric(as.factor(spx$Indep.Lead.Dir)))
-spx$Indep.Chrprsn <- (as.numeric(as.factor(spx$Indep.Chrprsn)))
-spx$CEO.Duality <- (as.numeric(as.factor(spx$CEO.Duality)))
-spx$Clssfd.Bd.Sys <- (as.numeric(as.factor(spx$Clssfd.Bd.Sys)))
-spx$Ticker <- NULL
-
-spx.scale <- data.frame(scale(spx, center = TRUE, scale = TRUE))
-dim(spx.scale)
 mydb_imputed_scaled  <- dbConnect(MySQL(), user='root', password='', dbname='corp_gov_imputed_scaled')
-dbWriteTable(mydb_imputed_scaled, value = spx.scale, name = "spx", overwrite = TRUE ) 
+mydb_raw <- dbConnect(MySQL(), user='root', password='', dbname='corp_gov')
+
+
+spx.imputed <- dbReadTable(conn=mydb_imputed,name='spx')
+spx.imputed$Feml.CEO.or.Equiv <- (as.numeric(as.factor(spx.imputed$Feml.CEO.or.Equiv)))
+spx.imputed$AZS.class <- (as.numeric(as.factor(spx.imputed$AZS.class)))
+spx.imputed$Frmr.CEO.or.its.Equiv.on.Bd <- (as.numeric(as.factor(spx.imputed$Frmr.CEO.or.its.Equiv.on.Bd)))
+spx.imputed$Prsdg.Dir <- (as.numeric(as.factor(spx.imputed$Prsdg.Dir)))
+spx.imputed$Indep.Lead.Dir <- (as.numeric(as.factor(spx.imputed$Indep.Lead.Dir)))
+spx.imputed$Indep.Chrprsn <- (as.numeric(as.factor(spx.imputed$Indep.Chrprsn)))
+spx.imputed$CEO.Duality <- (as.numeric(as.factor(spx.imputed$CEO.Duality)))
+spx.imputed$Clssfd.Bd.Sys <- (as.numeric(as.factor(spx.imputed$Clssfd.Bd.Sys)))
+#spx$Ticker <- NULL
+num.ind <- sapply(spx.imputed, is.numeric)
+f <-  function(dataset) {scale(dataset,center = TRUE, scale = TRUE)}
+spx.imputed[num.ind] <- lapply(spx.imputed[num.ind], f)
+
+
+spx.energy <- dbReadTable(conn=mydb_raw,name='spx_energy')
+spx.energy.con <- spx.energy[ which(spx.energy$Variable=='ENERGY_CONSUMPTION'), ]
+spx.energy.con <- data.frame(spx.energy.con$Ticker,spx.energy.con$X2014)
+names <- c("Ticker", "Energy.Consumption")
+colnames(spx.energy.con) <- names
+
+dummy <- spx.energy.con
+names <- c("Ticker", "Energy.Consumption.Again")
+colnames(dummy) <- names
+dummy.two <- merge(dummy, spx.energy.con, by="Ticker") 
+dim(dummy.two)
+
+
+spx.to.write <- merge(spx.imputed, spx.energy.con, by="Ticker", all=T) 
+dim(spx.to.write)
+
+dbWriteTable(mydb_imputed_scaled, value = spx, name = "spx", overwrite = TRUE,row.names=FALSE ) 
+summary(spx$Ticker)
