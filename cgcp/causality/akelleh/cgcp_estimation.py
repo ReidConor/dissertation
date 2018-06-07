a891f20f6bf666e0a9e303dcedd9b1f9e22bf283
Conor Reid <conor.reid@ucdconnect.ie>
2018-06-07 21:19:18 +0100

    more implementation

--- cgcp/causality/akelleh/cgcp_estimation.py
+++ cgcp/causality/akelleh/cgcp_estimation.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import os
 import MySQLdb
+from builtins import any as b_any
 from causality.estimation.parametric import PropensityScoreMatching
 
 #read in data from mysql
@@ -18,15 +19,39 @@ def getData(table):
     return (data, data_types)
 
 
+def stageAlgo(types):
+    desiredDataTypes = ['double']
+    exclude = ['AZS','Tobins.Q','Tobins.Q.class','AZS.class','Feml.CEO.or.Equiv']
+
+    variable_types = {}
+    for index, row in types.iterrows():
+        if b_any(row["Type"] in x for x in desiredDataTypes):
+            if row["Field"] not in exclude:
+                variable_types[row["Field"]]="c"
+
+    return (variable_types)
+
+
 if __name__ == "__main__":
     os.system('clear')
 
     data, types = getData("spx")
+    controlFor = stageAlgo(types)
 
     matcher = PropensityScoreMatching()
-    print(matcher.estimate_ATE(data, 'Feml.CEO.or.Equiv', 'Tobins.Q', {'P.EBITDA': 'c', 'P.B': 'c', 'Asset':'c'}, bootstrap=True))
+    print(matcher.estimate_ATE(data, 'Feml.CEO.or.Equiv', 'AZS', {'P.EBITDA': 'c', 'P.B': 'c', 'Asset':'c', 'Tax':'c', 'P.E':'c'}, bootstrap=True))
     #of the form matcher.estimate_ATE (data.frame, treatment, target, dict{vars to control for})
 
-    matcher.check_support(data, 'Feml.CEO.or.Equiv', {'P.EBITDA': 'c', 'P.B': 'c','Asset':'c'})
+    matcher.check_support(data, 'Feml.CEO.or.Equiv', {'P.EBITDA': 'c', 'P.B': 'c','Asset':'c', 'Tax':'c', 'P.E':'c'})
 
-    print(matcher.assess_balance(data, 'Feml.CEO.or.Equiv', {'P.EBITDA': 'c', 'P.B': 'c','Asset':'c'}))
+    print("")
+    print("Balance before matching")
+    print(matcher.assess_balance(data, 'Feml.CEO.or.Equiv', {'P.EBITDA': 'c', 'P.B': 'c','Asset':'c', 'Tax':'c', 'P.E':'c', 'propensity score': 'c'}))
+    print ("")
+
+    data = matcher.score(data, assignment='Feml.CEO.or.Equiv', confounder_types={'P.EBITDA': 'c', 'P.B': 'c','Asset':'c', 'Tax':'c', 'P.E':'c'})
+    treated, control = matcher.match(data, assignment='Feml.CEO.or.Equiv')
+    print("")
+    print("Balance after matching")
+    print(matcher.assess_balance(treated.append(control), 'Feml.CEO.or.Equiv', {'P.EBITDA': 'c', 'P.B': 'c','Asset':'c', 'Tax':'c', 'P.E':'c', 'propensity score': 'c'}))
+    print ("")
--- cgcp/causality/causalTree/causalTree.R
+++ cgcp/causality/causalTree/causalTree.R
@@ -1,42 +1,4 @@
 library(causalTree)
-library(RMySQL)
-
-#get data
-mydb.raw <- dbConnect(MySQL(), user='root', password='', dbname='corp_gov')
-mydb.processed <- dbConnect(MySQL(), user='root', password='', dbname='corp_gov_processed')
-spx <- dbReadTable(conn=mydb.processed,name='spx')
-spx.energy <- dbReadTable(conn=mydb.raw,name='spx_energy')
-
-spx.energy.consumption <- spx.energy[ which(spx.energy$Variable=='ENERGY_CONSUMPTION'), c("Ticker","X2014")]
-#replace NA's with mean (for now)
-spx.energy.consumption[is.na(spx.energy.consumption[,"X2014"]), "X2014"] <- 
-  mean(spx.energy.consumption[,"X2014"], na.rm = TRUE)
-
-#this wont work....give up on merge for now.... 
-spx.analysis <- merge(spx,spx.energy.consumption,by="Ticker")
-
-spx$treatment <- ifelse(spx$X..Wmn.on.Bd >= 3, 1, 0)
-spx[is.na(spx[,"treatment"]), "treatment"] <- 0
-
-tree <- causalTree(Tobins.Q.class ~ ., data = spx, treatment = spx$treatment,
-                   split.Rule = "CT", cv.option = "CT", split.Honest = T, cv.Honest = T, split.Bucket = F, 
-                   xval = 5, cp = 0, minsize = 20, propensity = 0.5)
-
-
-tree$cptable
-opcp <- tree$cptable[,1][which.min(tree$cptable[,4])]
-
-opfit <- prune(tree, opcp)
-
-rpart.plot(opfit)
-
-
-
-
-#---------------------------------
-
-
-library(causalTree)
 tree <- causalTree(y~ x1 + x2 + x3 + x4, data = simulation.1, treatment = simulation.1$treatment,
                    split.Rule = "CT", cv.option = "CT", split.Honest = T, cv.Honest = T, split.Bucket = F, 
                    xval = 5, cp = 0, minsize = 20, propensity = 0.5)
