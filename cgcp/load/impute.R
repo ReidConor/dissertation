e3f296b6a82ba6f1fcf3d9b3f3302d590de19cbe
Conor Reid <conor.reid@ucdconnect.ie>
2018-06-18 21:41:30 +0100

    more manips

--- cgcp/load/impute.R
+++ cgcp/load/impute.R
@@ -4,7 +4,7 @@
 library(RMySQL)
 mydb_processed <- dbConnect(MySQL(), user='root', password='', dbname='corp_gov_processed')
 spx <- dbReadTable(conn=mydb_processed,name='spx')
-spx.mscore <- dbReadTable(conn=mydb_processed,name='spx_mscore')
+spx.cgcp <- dbReadTable(conn=mydb_processed,name='spx_cgcp')
 sxxp <- dbReadTable(conn=mydb_processed,name='sxxp')
 eebp <- dbReadTable(conn=mydb_processed,name='eebp')
 
@@ -30,20 +30,26 @@ impute <- function(dataset){
   return(dataset.imputed.stacked)
 }
 spx.imputed <- impute(spx)
-spx.mscore.imputed <- impute(spx.mscore)
+spx.cgcp.imputed <- impute(spx.cgcp)
 sxxp.imputed <- impute(sxxp)
 eebp.imputed <- impute(eebp)
 
 
 #get complete cases
 spx.imputed.complete=spx.imputed[complete.cases(spx.imputed), ]
-spx.mscore.imputed.complete=spx.mscore.imputed[complete.cases(spx.mscore.imputed), ]
+spx.cgcp.imputed.complete=spx.cgcp.imputed[complete.cases(spx.cgcp.imputed), ]
 sxxp.imputed.complet=sxxp.imputed[complete.cases(sxxp.imputed), ]
 eebp.imputed.complete=eebp.imputed[complete.cases(eebp.imputed), ]
+
+#sort out the esg disc bin score
+esg.disc.mean <- mean(spx.cgcp.imputed.complete$ESG_DISCLOSURE_SCORE)
+spx.cgcp.imputed.complete["esg_disc_score_bin"] <- ifelse(spx.cgcp.imputed.complete$ESG_DISCLOSURE_SCORE > esg.disc.mean, 1, 0)
+spx.cgcp.imputed.complete$ESG_DISCLOSURE_SCORE <- NULL
+
 
 #write back to mysql
 mydb_imputed <- dbConnect(MySQL(), user='root', password='', dbname='corp_gov_imputed')
 dbWriteTable(mydb_imputed, value = spx.imputed.complete, name = "spx", overwrite = TRUE )
-dbWriteTable(mydb_imputed, value = spx.mscore.imputed.complete, name = "spx_mscore", overwrite = TRUE )
+dbWriteTable(mydb_imputed, value = spx.cgcp.imputed.complete, name = "spx_cgcp", overwrite = TRUE )
 dbWriteTable(mydb_imputed, value = sxxp.imputed.complet, name = "sxxp", overwrite = TRUE )
 dbWriteTable(mydb_imputed, value = eebp.imputed.complete, name = "eebp", overwrite = TRUE )
