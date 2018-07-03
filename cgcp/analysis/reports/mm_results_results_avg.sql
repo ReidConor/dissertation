9ec3f9de5f6066dca296eb56c3822c6665a75974
Conor Reid <conor.reid@ucdconnect.ie>
2018-07-03 19:40:39 +0100

    new handy sql

--- cgcp/analysis/reports/mm_results_results_avg.sql
+++ cgcp/analysis/reports/mm_results_results_avg.sql
@@ -4,11 +4,11 @@ select
   a.Algorithm,
   a.DataSet,
   a.Target,
-  avg(a.`Correctly Classified Instances`),
-  avg(a.`Coverage Of cases`),
-  avg(a.`Precision Class 0`),
-  avg(a.`Precision Class 1`),
-  avg(a.`ROC area`)
+  avg(a.`Correctly Classified Instances`) avg_accuracy,
+  avg(a.`Coverage Of cases`) as avg_coverage,
+  avg(a.`Precision Class 0`) as avg_precision_0,
+  avg(a.`Precision Class 1`) as avg_precision_1,
+  avg(a.`ROC area`) as avg_roc_area
 from mm_results.results a
 group by
   a.Algorithm,
